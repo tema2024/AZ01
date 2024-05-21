1471cff0274c270e7b937b1167b5d6ed7a9adafb
tema2024 <163888751+tema2024@users.noreply.github.com>
2024-05-21 19:11:36 +0300

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import pandas as pd
+
+data = pd.read_csv('dz.csv')
+
+average_salary_by_city = data.groupby('City')['Salary'].mean().reset_index()
+
+print(average_salary_by_city)
